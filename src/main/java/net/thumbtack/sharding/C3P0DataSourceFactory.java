a336a5af9d3e3ec86f38f89881ce906055bda55a
aremnev <aremnev@thumbtack.net>
2013-04-05 13:52:31 +0700

    And now even compilable :)

--- src/main/java/net/thumbtack/sharding/C3P0DataSourceFactory.java
+++ src/main/java/net/thumbtack/sharding/C3P0DataSourceFactory.java
@@ -1,4 +1,4 @@
-package net.thumbtack.onepm.dao.ibatis;
+package net.thumbtack.sharding;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
