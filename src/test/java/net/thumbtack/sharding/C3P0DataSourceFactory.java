a7166c699dd9c940d36edf0b6a0945243cb19072
Anton Tarasenko <zymosi3@gmail.com>
2013-04-09 18:01:03 +0700

    remove BaseDaoImpl to tests

---
+++ src/test/java/net/thumbtack/sharding/C3P0DataSourceFactory.java
@@ -0,0 +1,20 @@
+package net.thumbtack.sharding;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
+
+import java.util.Properties;
+
+public class C3P0DataSourceFactory extends UnpooledDataSourceFactory {
+
+	public C3P0DataSourceFactory() {
+		// disable verbose internal logging of pool
+		Properties p = new Properties(System.getProperties());
+		p.put("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
+		p.put("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "WARNING");
+		System.setProperties(p);
+
+		this.dataSource = new ComboPooledDataSource();
+	}
+
+}
