af00692b3f71ab38594491633744993cfda558b5
Anton Tarasenko <zymosi3@gmail.com>
2013-05-07 14:15:30 +0700

    vbucket migration

--- src/main/java/net/thumbtack/sharding/vbucket/VbucketMigration.java
+++ src/main/java/net/thumbtack/sharding/vbucket/VbucketMigration.java
@@ -3,11 +3,11 @@ package net.thumbtack.sharding.vbucket;
 import fj.F;
 import net.thumbtack.helper.NamedThreadFactory;
 import net.thumbtack.sharding.core.Shard;
-import net.thumbtack.sharding.core.query.Connection;
-import net.thumbtack.sharding.core.query.QueryClosure;
+import net.thumbtack.sharding.core.query.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -18,12 +18,18 @@ public class VbucketMigration {
 
     private static final Logger logger = LoggerFactory.getLogger(VbucketMigration.class);
 
+    private final VbucketMapper mapper;
+    private final Map<Integer, Shard> shards;
     private VbucketMigrationHelper helper;
+    private MigrationFinishedListener finishedListener;
 
     private ExecutorService executor;
 
-    public VbucketMigration(VbucketMigrationHelper migrationHelper) {
+    public VbucketMigration(VbucketMapper mapper, Map<Integer, Shard> shards, VbucketMigrationHelper migrationHelper, MigrationFinishedListener finishedListener) {
+        this.mapper = mapper;
+        this.shards = shards;
         this.helper = migrationHelper;
+        this.finishedListener = finishedListener;
         executor = Executors.newSingleThreadExecutor(new NamedThreadFactory("chunk-migration"));
     }
 
@@ -33,44 +39,58 @@ public class VbucketMigration {
             @Override
             public Void call() throws Exception {
 
-                final Connection connFrom = getBucketConnection(bucket.id);
-                final Connection connTo = getShardConnection(toShard);
+                final List<Connection> connFrom = getBucketConnection(bucket);
+                final List<Connection> connTo = getShardConnection(toShard);
+                final Query select = new SelectSpecShard();
+                final Query update = new UpdateSpecShard();
                 final QueryClosure<Long> lastModificationF = helper.getLastModificationTime(bucket.idFrom, bucket.idTo);
 
-                F<Long, Void> migrationStep = new F<Long, Void>() {
+                F<Long, Boolean> migrationStep = new F<Long, Boolean>() {
                     @Override
-                    public Void f(Long timeFrom) {
+                    public Boolean f(Long timeFrom) {
                         try {
-                            long lastModificationTime = lastModificationF.call(connFrom);
+                            long lastModificationTime = select.query(lastModificationF, connFrom);
                             QueryClosure<List<Long>> modifiedAfterF = helper.getModifiedAfter(timeFrom);
-                            List<Long> ids = modifiedAfterF.call(connFrom);
+                            List<Long> ids = select.query(modifiedAfterF, connFrom);
                             if (ids == null || ids.isEmpty()) {
-                                return null;
+                                return true;
                             }
 
                             for (long id : ids) {
-                                Object entity = helper.getEntity(id).call(connFrom);
-                                helper.setEntity(entity).call(connTo);
+                                Object entity = select.query(helper.getEntity(id), connFrom);
+                                update.query(helper.setEntity(entity), connTo);
                             }
 
                             return f(lastModificationTime);
                         } catch (Exception e) {
                             logger.error("Migration failed with error", e);
-                            return null;
+                            return false;
                         }
                     }
                 };
 
-                return migrationStep.f(0L);
+                boolean finished = migrationStep.f(0L);
+                if (finished && finishedListener != null) {
+                    finishedListener.onFinish(bucket.id, toShard);
+                }
+                return null;
             }
         });
     }
 
-    private Connection getShardConnection(int shradId) {
-        return null;  //To change body of created methods use File | Settings | File Templates.
+    private List<Connection> getShardConnection(int shardId) {
+        Shard shard = shards.get(shardId);
+        List<Connection> connections = new ArrayList<Connection>(1);
+        connections.add(shard.getConnection());
+        return connections;
     }
 
-    private Connection getBucketConnection(int bucketId) {
-        return null;  //To change body of created methods use File | Settings | File Templates.
+    private List<Connection> getBucketConnection(Vbucket bucket) {
+        int shardId = mapper.shard(bucket.idFrom);
+        return getShardConnection(shardId);
+    }
+
+    public interface MigrationFinishedListener {
+        void onFinish(int bucketId, int toShard);
     }
 }
